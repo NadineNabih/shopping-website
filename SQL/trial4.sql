a44bfa9c14947287f6c6770e4474aec78c179fd3
NadineNabih <97252362+NadineNabih@users.noreply.github.com>
2025-01-04 16:32:52 +0200

    starting good work i hope

--- SQL/trial4.sql
+++ SQL/trial4.sql
@@ -5,4 +5,23 @@ CREATE TABLE  product_gender(
     CONSTRAINT pk_prodgender
     PRIMARY KEY (product_gender_id)
 );
-INSERT INTO
+INSERT INTO product_gender(product_gender_id,gender_name)VALUES
+(1,"women"),
+(2,"men");
+--@block
+CREATE TABLE product_category(
+    product_category_id INT,
+    category_name VARCHAR(100),
+    category_image VARCHAR(400),
+    product_gender_id INT,
+    CONSTRAINT pk_productcategory
+    PRIMARY KEY(product_category_id),
+    CONSTRAINT fk_product_gender
+    FOREIGN KEY (product_gender_id)REFERENCES product_gender(product_gender_id)
+);
+INSERT INTO product_category(product_category_id,category_name,category_image,product_gender_id)VALUES
+--@martinaarmanios fill the data according to front 
+(1,'makeup','path/to/image.extension',1),
+(2,'dresses','path/to/inage.extension'1),
+(3,'');
+
